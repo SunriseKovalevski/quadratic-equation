ec72c895388344b9f7422c935553b5dd00aeef20
SunriseKovalevski <Sunrisekovalevsky283@gmail.com>
2018-09-24 17:33:27 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr=equation.split(' '), 
+  	a = +arr[0], 
+  	b = +arr[4], 
+  	c = +arr[8],
+  	result = [];
+
+  if (arr[3] == '-') b = -b;
+  if (arr[7] == '-') c = -c;
+
+  var Discr = Math.sqrt(b * b - 4 * a * c);
+
+  var x1=Math.round((-b - Discr) / (2 * a)), 
+  	x2=Math.round((-b + Discr) / (2 * a));
+
+  result.push(Math.round((-b - Discr) / (2 * a)));
+  result.push(Math.round((-b + Discr) / (2 * a)));
+
+  function compare(a, b){
+    return a - b;
+  }
+
+  return result.sort(compare);
 }
